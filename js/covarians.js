723396c92dda46fbad54081d467250cdacb32ab0
sadegh <thesadq.80@gmail.com>
2023-11-06 14:19:46 +0330

    upadte

--- js/covarians.js
+++ js/covarians.js
@@ -5,43 +5,34 @@ const showCovarians = document.querySelector("#covarians");
 const covResetBtn = document.querySelector("#cov-reset-btn")
 
 covResetBtn.addEventListener("click", function () {
-  collection1.value = ""
-  collection2.value = ""
-  showCovarians.innerHTML = ""
-})
+  collection1.value = "";
+  collection2.value = "";
+  showCovarians.innerHTML = "";
+});
 
-// تعریف دو مجموعه اولیه
 let dataSet1 = [];
 let dataSet2 = [];
 
-// تابع برای محاسبه کوواریانس
 function calculateCovariance() {
-  // خواندن داده ها از کاربر
-  let input1 = collection1.value.split(' ');
-  let input2 = collection2.value.split(' ');
+  const input1 = collection1.value.split(" ").map(Number);
+  const input2 = collection2.value.split(" ").map(Number);
 
-  // تبدیل رشته به اعداد
-  dataSet1 = input1.map(Number);
-  dataSet2 = input2.map(Number);
-
-  // بررسی طول مجموعه ها
-  if (dataSet1.length !== dataSet2.length) {
+  if (input1.length !== input2.length) {
     showCovarians.innerHTML = "تعداد اعداد مجموعه باید برابر باشند";
+    return; // Exit the function if lengths are not equal
   }
 
-  // محاسبه میانگین مجموعه ها
-  let sum1 = dataSet1.reduce((acc, val) => acc + val, 0);
-  let mean1 = sum1 / dataSet1.length;
+  const sum1 = input1.reduce((acc, val) => acc + val, 0);
+  const mean1 = sum1 / input1.length;
 
-  let sum2 = dataSet2.reduce((acc, val) => acc + val, 0);
-  let mean2 = sum2 / dataSet2.length;
+  const sum2 = input2.reduce((acc, val) => acc + val, 0);
+  const mean2 = sum2 / input2.length;
 
-  // محاسبه کوواریانس
   let covariance = 0;
-  for (let i = 0; i < dataSet1.length; i++) {
-    covariance += (dataSet1[i] - mean1) * (dataSet2[i] - mean2);
+  for (let i = 0; i < input1.length; i++) {
+    covariance += (input1[i] - mean1) * (input2[i] - mean2);
   }
-  covariance /= (dataSet1.length - 1);
+  covariance /= input1.length - 1;
 
   showCovarians.innerHTML = `کوواریانس ${covariance} است`;
 }
---
+++ js/regression.js
@@ -0,0 +1,73 @@
+const actualY = document.querySelector("#actualY");
+const hatY = document.querySelector("#predictedY");
+const regResult = document.querySelector("#reg-result");
+const regResetBtn = document.querySelector("#reg-reset-btn");
+const regSSE = document.querySelector("#reg-sse");
+const regSST = document.querySelector("#reg-sst");
+const regSSR = document.querySelector("#reg-ssr");
+const regR = document.querySelector("#reg-r");
+
+regResult.addEventListener("click", calcRegression);
+regResetBtn.addEventListener("click", resetFields);
+
+function calcRegression() {
+  const yValues = getInputValues(actualY);
+  const predictedValues = getInputValues(hatY);
+
+  const meanY = calculateMean(yValues);
+  const sst = calculateSST(yValues, meanY);
+  const sse = calculateSSE(predictedValues, meanY);
+  const ssr = calculateSSR(sst, sse);
+  const rSquare = calculateRSquare(sse, sst);
+
+  displayResults(sst, sse, ssr, rSquare);
+}
+
+function resetFields() {
+  clearInputFields(actualY, hatY);
+  clearOutputFields(regSSE, regSST, regSSR, regR);
+}
+
+function getInputValues(inputField) {
+  const inputValues = inputField.value.split(" ");
+  return inputValues.map(Number);
+}
+
+function calculateMean(values) {
+  const sum = values.reduce((acc, curr) => acc + curr, 0);
+  return sum / values.length;
+}
+
+function calculateSST(values, mean) {
+  return values.reduce((acc, curr) => acc + Math.pow(curr - mean, 2), 0);
+}
+
+function calculateSSE(predictedValues, mean) {
+  return predictedValues.reduce(
+    (sum, prediction) => sum + Math.pow(prediction - mean, 2),
+    0
+  );
+}
+
+function calculateSSR(sst, sse) {
+  return sst - sse;
+}
+
+function calculateRSquare(sse, sst) {
+  return sse / sst;
+}
+
+function displayResults(sst, sse, ssr, rSquare) {
+  regSST.innerHTML = `SST: ${sst}`;
+  regSSE.innerHTML = `SSE: ${sse}`;
+  regSSR.innerHTML = `SSR: ${ssr}`;
+  regR.innerHTML = `R^2: ${rSquare}`;
+}
+
+function clearInputFields(...fields) {
+  fields.forEach((field) => (field.value = ""));
+}
+
+function clearOutputFields(...fields) {
+  fields.forEach((field) => (field.innerHTML = ""));
+}
